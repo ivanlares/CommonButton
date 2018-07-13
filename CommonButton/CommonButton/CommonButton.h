73c4063e8be0babc2984cff09d4abe0005f39d80
ivan lares <ivanlares1@gmail.com>
2018-07-13 11:51:12 -0700

    initial commit

---
+++ CommonButton/CommonButton/CommonButton.h
@@ -0,0 +1,19 @@
+//
+//  CommonButton.h
+//  CommonButton
+//
+//  Created by ivan lares on 7/13/18.
+//  Copyright © 2018 ivan lares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CommonButton.
+FOUNDATION_EXPORT double CommonButtonVersionNumber;
+
+//! Project version string for CommonButton.
+FOUNDATION_EXPORT const unsigned char CommonButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonButton/PublicHeader.h>
+
+
---
+++ ExampleProject/ExampleProject/StoryboardExampleController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleProject
+//
+//  Created by ivan lares on 7/13/18.
+//  Copyright © 2018 ivan lares. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
